7623a7e152dfcf03c33ce21cdf0a2143c5e31999
Raghul <raghulchandramouli@gmail.com>
2025-07-15 20:37:04 +0530

    Estimation loss feature added

--- nanoGPT/teenygpt.py
+++ nanoGPT/teenygpt.py
@@ -52,8 +52,26 @@ def get_batch(split):
     
     data = train_data if split == "train" else val_data
     ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i + block_size+1] for i in ix])
+    y = torch.stack([data[i + 1 : i + block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
     
-    pass
+    return x, y
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits. loss = model(X, Y)
+            losses[k] = loss.item()
+        
+        out[split] = losses.mean()
+    model.train()
+    return out
 
 
     
