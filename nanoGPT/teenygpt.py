d9be46f162d86f414df4ea988a94c088e3478cd3
Raghul <raghulchandramouli@gmail.com>
2025-07-15 20:43:06 +0530

    created a single Head

--- nanoGPT/teenygpt.py
+++ nanoGPT/teenygpt.py
@@ -73,6 +73,41 @@ def estimate_loss():
     model.train()
     return out
 
+class Head(nn.Module):
+    
+    """
+    One head of self-attention
+    """
+    
+    def __init__(self,
+                 head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias = False)
+        self.value = nn.Linear(n_embd, head_size, bias = False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
 
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        
+        # I/P of Size (batch, time-out, channels) 
+        # O?P of Size (batch, time-step, head-size)
+        
+        B,T,C = x.shape
+        k = self.key(x) 
+        q = self.query(x)
+        
+        # we compute affinity (`attn score`)
+        wei = q @ k.transpose(-2, -1) * k.shape[-1] ** -0.5
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        wei = F.softmax(wei, dim = -1)
+        wei = self.dropout(wei)
+        
+        # perform the weighted aggregation of the vals
+        v = self.value(x)
+        out = wei @ v
+        return out
+    
     
     
